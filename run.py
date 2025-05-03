45fd962228a7a794df8859d83668d4edac8758ca
currypurin <hiroisu@gmail.com>
2025-05-03 16:00:52 +0900

    refactor: YOUTUBE_CHANNEL_IDの表示を削除

--- run.py
+++ run.py
@@ -15,11 +15,8 @@ def main():
     print("\n=== 環境変数の確認 ===")
     server_host = os.getenv('SERVER_HOST')
     youtube_api_key = os.getenv('YOUTUBE_API_KEY')
-    print(youtube_api_key)
-    youtube_channel_id = os.getenv('YOUTUBE_CHANNEL_ID')
 
     print(f"SERVER_HOST: {server_host or '未設定'}")
-    print(f"YOUTUBE_CHANNEL_ID: {youtube_channel_id or '未設定'}")
     print(f"YOUTUBE_API_KEY: {'設定済み' if youtube_api_key else '未設定'}")
     print("==================\n")
 
